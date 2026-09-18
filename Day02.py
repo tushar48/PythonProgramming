a16009eba451bbe13b3cb29a6ead45e3b2a2b6c7
Tushar Kumar <tusharchamp777@gmail.com>
2026-09-18 18:32:44 +0530

    Day 2

---
+++ Day02.py
@@ -0,0 +1,22 @@
+'''False
+None
+0
+0.0
+""
+[]
+{}
+set()'''
+
+#Above all values are falsy
+name = ""
+if name:
+    print(1)
+else:
+    print(0)
+    
+age = 25
+
+if 18 <= age <= 60:
+    print(1)
+else:
+    print(0)
